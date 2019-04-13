1194af3952dca323d16c9c77718832e538602410
Rodrigo Caldas <rodrigotmcaldas@comp.ufla.br>
2019-04-13 16:36:52 -0300

    verificação de números primos já paralelizados

---
+++ crivo.c
@@ -0,0 +1,71 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <locale.h>
+#define NUM 100000000
+#define NUM_THREAD 4
+
+/*												LÓGICA DO PROGRAMA
+Ter um vetor em que cada posição represente o número posterior a ele (posição 0 representa o número 1, posição 99 representa o número 100),
+cada posição do vetor começa com um valor igual a 1, deste modo estamos assumindo que todos os números são primos e devemos provar o contrário.
+A variável k é o número que pegamos como base para marcar todos os seus múltiplos como não primos(alterando o valor dentro do vetor para 0),
+o valor de k é a id da thread+1, dessa forma pulamos o número 1 como é preciso pular e a variável é acrescida pelo número de threads criadas.
+Com 4 Threads criada: T1 começa com k=1, depois k=5, depois k=9. T2 começa com k=2, depois k=6, depois k=10. E assim por diante.*/
+
+/* Realizando uma média de tempo de execução depois de 3 execuções do programa, com NUM igual a 100.000.000 (Sem a impressão dos valores primos):
+Com 2 threads: 9,7713
+Com 3 threads: 9,6660
+Com 4 threads: 9.7030*/
+
+int vetor[NUM];
+
+/*Função responsável por pegar um valor base (k) e marcar todos os seus múltiplos como não primos, a partir de k²*/
+void *funcao_crivo(void *arg){
+	//printf("thread %d mandando um olá.\n",pthread_self());
+	//Cada thread possui um k próprio pulando o k=1
+	int k=pthread_self()+1;
+	int i;
+	//printf("O valor de k é: %d na thread: %d\n",k,pthread_self());
+	
+	//A partir de k até o valor máximo divido por k, altera no vetor o valor para 0 dos múltiplos de k
+	while(k <= sqrt(NUM)) {
+		//printf("O valor de k é: %d na thread: %d\n",k,pthread_self());
+		for(i = k; i <= (NUM/k); i++){
+			//printf("estamos no i:%d\n",i);
+			vetor[(k*i)-1]=0;
+		}	
+		k=k+NUM_THREAD;
+	}
+}
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");	
+	int i;	
+	pthread_t crivo[NUM_THREAD];
+	
+	//Assume-se em primeira instância que todos os números são primos, logo toda posição do vetor recebe o valor 1
+	for(i=0;i<NUM;i++){
+		vetor[i]=1;
+	}
+	
+	//Criação das threads
+	for(i=0;i<NUM_THREAD;i++){
+		pthread_create(&crivo[i],NULL,funcao_crivo,NULL);
+	}	
+	//Encerramento das threads
+	for(i=0;i<NUM_THREAD;i++){
+		pthread_join(crivo[i],NULL);
+	}
+
+	//Imprime apenas os números primos	
+	/*printf ("Os números primos até %d são: ",NUM);
+	for(i=0;i<NUM;i++){
+		if(vetor[i]==1){
+			printf("%d ",i+1);	
+		}
+	}*/
+	
+	return 0;
+}
